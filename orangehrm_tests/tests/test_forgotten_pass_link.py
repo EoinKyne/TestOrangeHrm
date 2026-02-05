6dc575004fd7bae1fce670df480fbe4c39a1192f
ekyne <eoin_kyne@yahoo.com>
2026-02-05 14:47:17 +0000

    adding tests verify socials links on login page

--- orangehrm_tests/tests/test_forgotten_pass_link.py
+++ orangehrm_tests/tests/test_forgotten_pass_link.py
@@ -5,5 +5,5 @@ import re
 
 def test_verify_forgotten_password_link(page):
     login_page = LoginPage(page)
-    login_page.verifyForgotPasswordLink()
+    login_page.verify_forgot_password_link()
     expect(page).to_have_url(re.compile("requestPasswordResetCode"))
